c8ecd215b432edf5f361666088f24366cfb94825
danyvassily <116579851+danyvassily@users.noreply.github.com>
2024-11-12 20:22:23 +0100

    connection à la base de données

--- mern/backend/index.js
+++ mern/backend/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose 
+.connect(process.env.MONGO)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((error) => console.log('Connexion à MongoDB échouée !', error));
 
 const app = express();
 
