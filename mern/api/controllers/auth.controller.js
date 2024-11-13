717cf4109ba613a786c304d33e6b6d05bfe1f4d9
danyvassily <116579851+danyvassily@users.noreply.github.com>
2024-11-13 18:09:42 +0100

    partie auth fonction et middleware pour la creation d'user et le hashing avec bcryptjs

--- mern/api/controllers/auth.controller.js
+++ mern/api/controllers/auth.controller.js
@@ -1,3 +1,21 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+
 export const signup = async (req, res) => {
- console.log(req.body);
+    const {username, email, password} = req.body;
+
+    if(!username || !email || !password || username === "" || email === "" || password === "") {
+        return res.status(400).json({message: "Tous les champs sont requis"});
+    }
+    // hashage du mot de passe
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({username, email, password: hashedPassword});
+    try {
+        await newUser.save();
+        res.status(201).json("Utilisateur crée avec succès");
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 };
--- mern/api/index.js
+++ mern/api/index.js
@@ -1,21 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+
+// configuration de dotenv
 dotenv.config();
 
-mongoose 
-.connect(process.env.MONGO)
-.then(() => console.log('Connexion à MongoDB réussie !'))
-.catch((error) => console.log('Connexion à MongoDB échouée !', error));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connecté à MongoDB avec succès");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à MongoDB:", err);
+    process.exit(1);
+  });
 
+// configuration de l'application express
 const app = express();
 
+// middleware pour les requettes entrante en json
+app.use(express.json());
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute);
+// configuration du port
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Le serveur est en marche sur le port 3000');
+app.listen(PORT, () => {
+  console.log(`Le serveur est en marche sur le port ${PORT}`);
 });
+
+// configuration des routes
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
