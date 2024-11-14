fdad1c2012a7697dd25f85b720963c986f6b9fd4
danyvassily <116579851+danyvassily@users.noreply.github.com>
2024-11-14 20:21:19 +0100

    create signin api route

--- mern/api/controllers/auth.controller.js
+++ mern/api/controllers/auth.controller.js
@@ -1,23 +1,55 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-
+// Signup
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-        return next(errorHandler(400, "Tous les champs sont requis"));
+  if (!username || !email || !password) {
+    return next(errorHandler(400, "Tous les champs sont requis"));
+  }
+
+  // Hashage du mot de passe
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("Utilisateur créé avec succès");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Signin
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "Tous les champs sont requis"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Utilisateur non trouvé"));
     }
 
-    // Hashage du mot de passe
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-
-    const newUser = new User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(201).json("Utilisateur créé avec succès");
-    } catch (error) {
-        next(error);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Mot de passe incorrect"));
     }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
