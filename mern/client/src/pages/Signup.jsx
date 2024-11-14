6c8f6897de1d514f01c18c46aa4a34d76a6f6275
danyvassily <116579851+danyvassily@users.noreply.github.com>
2024-11-14 15:14:26 +0100

    add functionality to the sign up page

--- mern/client/src/pages/Signup.jsx
+++ mern/client/src/pages/Signup.jsx
@@ -1,8 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate} from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if(formData.username === '' || formData.email === '' || formData.password === '') {
+      setErrorMessage('Veuillez remplir tous les champs');
+      setLoading(false);
+      return;
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+   
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/signin');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+    setLoading(false);
+  };
+
   return (
+
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
@@ -11,7 +52,7 @@ export default function SignUp() {
             <span className="px-2 py-1 bg-gradient-to-r from-green-500 to-blue-500 rounded-lg text-white">
               SOCIAL
             </span>
-            Hub
+            Link
           </Link>
           <p className="text-sm mt-5">
             Bienvenue sur Social Hub, mon projet. Vous pouvez créer un compte ou vous connecter avec votre compte google.
@@ -21,23 +62,21 @@ export default function SignUp() {
         
         <div className="flex-1">
    
-            <form className="flex flex-col gap-4">
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
             <Label value="Nom d'utilisateur" />
             <TextInput
             type="text"
-            placeholder="Nom d'utilisateur"
-            id="username"
-            required
+            placeholder="Votre nom d'utilisateur"
+            id="username" onChange={handleChange}
             />
             </div>
             <div>
-            <Label value="votre email" />
+            <Label value="Votre email" />
             <TextInput
             type="text"
-            placeholder="votre email"
-            id="email"
-            required
+            placeholder="Vladimir@gmail.com"
+            id="email" onChange={handleChange}
             />
             </div>
             <div>
@@ -45,12 +84,17 @@ export default function SignUp() {
             <TextInput
             type="password"
             placeholder="Mot de passe"
-            id="password"
-            required
+            id="password" onChange={handleChange}
             />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit">
-              Sing up
+            <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className="pl-3">Chargement...</span>
+                </>
+              )
+              : ('S\'inscrire')}
             </Button>
           </form>
           <div className="div flex gap-2 text-sm mt-5">
@@ -59,6 +103,13 @@ export default function SignUp() {
               Connectez-vous
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )
+          
+          }
          
     
             
